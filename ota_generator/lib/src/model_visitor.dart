f177804e90c335556f0fb18c9388cc6a2c352b34
Ecko Huynh <>
2026-03-25 14:33:07 +0800

    chore: upgrade Flutter/Dart constraints and analyzer compatibility
    Align ota_string and ota_generator with newer Flutter/Dart toolchains, refresh dependency locks, and update generator visitor APIs for analyzer 8+ compatibility while syncing docs/changelogs.

--- ota_generator/lib/src/model_visitor.dart
+++ ota_generator/lib/src/model_visitor.dart
@@ -1,14 +1,14 @@
-import 'package:analyzer/dart/element/visitor.dart';
 import 'package:analyzer/dart/element/element.dart';
+import 'package:analyzer/dart/element/visitor2.dart';
 
-class ModelVisitor extends SimpleElementVisitor<dynamic> {
+class ModelVisitor extends RecursiveElementVisitor2<void> {
   late String className;
 
   Map<String, dynamic> fields = <String, dynamic>{};
 
   @override
-  dynamic visitConstructorElement(ConstructorElement element) {
-    final elementReturnType = element.type.returnType.toString();
+  void visitConstructorElement(ConstructorElement element) {
+    final elementReturnType = element.returnType.toString();
 
     // DartType ends with '*', which needs to be eliminated
     // for the generated code to be accurate.
@@ -16,11 +16,12 @@ class ModelVisitor extends SimpleElementVisitor<dynamic> {
   }
 
   @override
-  dynamic visitFieldElement(FieldElement element) {
+  void visitFieldElement(FieldElement element) {
     final elementType = element.type.toString();
 
     // DartType ends with '*', which needs to be eliminated
     // for the generated code to be accurate.
-    fields[element.name] = elementType.replaceFirst('*', '');
+    final fieldName = element.name ?? element.displayName;
+    fields[fieldName] = elementType.replaceFirst('*', '');
   }
 }
